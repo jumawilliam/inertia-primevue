ad06911a64a5a61be1bb82b9bcb2e238208e2708
william <jumawilliam2011@gmail.com>
2024-01-03 14:23:18 +0300

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Button from 'primevue/button';
+import Chips from 'primevue/chips';
+import {ref} from 'vue';
+const value =ref([])
+
+</script>
+<template>
+<Button label="Submit" /> <br>
+<Chips v-model="value" separator=","  />
+
+
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,19 @@
+import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/lara-light-green/theme.css'
+
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    return pages[`./Pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(PrimeVue)
+      .mount(el)
+  },
+})
